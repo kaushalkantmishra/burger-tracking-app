93745f812f2b321be7911082ff4533a87bdcbff9
kaushalkantmishra <kaushalkantmishra127@gmail.com>
2024-07-14 18:21:38 +0530

    fully functional

--- src/firebase.js
+++ src/firebase.js
@@ -10,13 +10,13 @@ import {
 } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAnZGPjY0Z_3IbiTq4wK-LMMAKMqDlmhd4",
-  authDomain: "tracking-app-87463.firebaseapp.com",
-  projectId: "tracking-app-87463",
-  storageBucket: "tracking-app-87463.appspot.com",
-  messagingSenderId: "977028649331",
-  appId: "1:977028649331:web:923b2c44edfbc11c613506",
-  measurementId: "G-04WNJP4JXZ",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
